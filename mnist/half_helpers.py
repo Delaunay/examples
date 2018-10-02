0f7889d645e10b8dfd272a14abe67f9a13a9b367
Pierre Delaunay <pierre.delaunay@mila.quebec>
2018-10-02 15:15:57 -0400

    add mixed precision support for the MNIST example

---
+++ mnist/half_helpers.py
@@ -0,0 +1,82 @@
+import torch
+
+
+global_use_gpu = False
+global_use_half = False
+
+
+def set_use_gpu(val):
+    global global_use_gpu
+    global_use_gpu = torch.cuda.is_available() and val
+
+
+def set_use_half(val):
+    global global_use_half
+    global_use_half  = val
+
+
+def use_gpu() -> bool:
+    global global_use_gpu
+    return global_use_gpu
+
+
+def use_half() -> bool:
+    global global_use_gpu
+    global global_use_half
+
+    return global_use_gpu and global_use_half
+
+
+def enable_cuda(object):
+    if use_gpu():
+        return object.cuda()
+
+    return object
+
+
+def enable_half(object):
+    if not use_gpu():
+        return object
+
+    if not use_half():
+        return object.cuda()
+
+    # F32 Tensor
+    try:
+        if object.dtype == torch.float32:
+            return object.cuda().half()
+    except:
+        # Not a tensor
+        return object.cuda().half()
+
+    # different type
+    return object.cuda()
+
+
+class OptimizerAdapter:
+    def __init__(self, optimizer, half=False, *args, **kwargs):
+        if half:
+            import apex.fp16_utils.fp16_optimizer as apex_optimizer
+
+            self.optimizer = apex_optimizer.FP16_Optimizer(optimizer, *args, **kwargs)
+        else:
+            self.optimizer = optimizer
+
+        self.half = half
+
+    def backward(self, loss):
+        if loss is None:
+            raise RuntimeError('')
+
+        if self.half:
+            self.optimizer.backward(loss)
+        else:
+            loss.backward()
+
+        return self.optimizer
+
+    def step(self):
+        return self.optimizer.step()
+
+    def zero_grad(self):
+        return self.optimizer.zero_grad()
--- mnist/main.py
+++ mnist/main.py
@@ -1,10 +1,15 @@
 from __future__ import print_function
 import argparse
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+
+import half_helpers as tools
+
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -24,19 +29,21 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
+
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = tools.enable_half(data.to(device)), tools.enable_half(target.to(device))
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
-        loss.backward()
+        optimizer.backward(loss)
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('\rTrain Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+                100. * batch_idx / len(train_loader), loss.item()), end='')
+
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -44,7 +51,7 @@ def test(args, model, device, test_loader):
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            data, target = tools.enable_half(data.to(device)), tools.enable_half(target.to(device))
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
@@ -54,6 +61,7 @@ def test(args, model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
 
 def main():
     # Training settings
@@ -74,8 +82,17 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
+    parser.add_argument('--data', type=str, default='../data', metavar='D',
+                        help='location where the data is downloaded and read from')
+    parser.add_argument('--half', action='store_true', default=False,
+                        help='enable half precision training')
+    parser.add_argument('--static_loss_scale', type=float, default=128, metavar='L',
+                        help='loss scaling constant when half precision mode is used')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    tools.set_use_gpu(use_cuda)
+    tools.set_use_half(args.half)
 
     torch.manual_seed(args.seed)
 
@@ -83,26 +100,36 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
+        datasets.MNIST(args.data, train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
+
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+        datasets.MNIST(args.data, train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    model = tools.enable_half(Net().to(device))
 
-    model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    # half mode need a special optimizer for loss rescaling
+    optimizer = tools.OptimizerAdapter(optimizer, half=tools.use_half(), static_loss_scale=args.static_loss_scale)
+    # static_loss_scale=16384.0 / 2
+    # dynamic_loss_scale=True, dynamic_loss_args={'init_scale': 128, 'scale_window': 5000})
+
+    start_time = time.time()
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
+
+    end_time = time.time()
+    print('Trained in {:4.2f} s'.format(end_time - start_time))
 
 
 if __name__ == '__main__':
